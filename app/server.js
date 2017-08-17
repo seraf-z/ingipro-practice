e9c514a0cb562f736d019e3b6396a8757c22d279
seraf-z <oleg.lebedev.v@gmail.com>
2017-08-17 23:37:50 +0300

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,96 +1,92 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+//const randomColor = require('randomcolor');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const PORT = 3000;
-const usersArr = [];
-const confArr = [];
+const PORT = 3002;
+const allStates = []; // все конференции сервера
+
 
 server.listen(PORT, () => {
-    //console.log(`Server started on localhost:${PORT}`);
+    console.log(`server listen ${PORT}`);
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/start.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/startStateOnServ.html');
 });
 
 io.sockets.on('connection', (socket) => {
-    const user = {};
-    user.id = Math.floor(Math.random() * 1000);
-    //console.log(`user connected ID ${user.id}`);
 
-    socket.on('userName', data => {
-        const a = usersArr.some(item => {
-            return item.name === data;
-        });
+  socket.uuid = Math.floor(Math.random() * 1000);
+  //socket.color = randomColor();
 
-        if (a) {
-            socket.emit('changeName');
-        } else {
-            user.name = data;
-            //console.log(`user ID ${user.id}. User name ${user.name}. Socket ID ${socket.id}`);
-            usersArr.push(user);
-            socket.emit('userNameConfirm', user);
-        }
-    });
+  socket
+    .on('message', data => {
 
-    socket.on('createConf', data => {
-        const a = confArr.some(item => { //проверка уникальности имени конференции
-            return item === data;
-        });
+      let type = data.type;
 
-        if (a) {
-            socket.emit('changeСonfName');
-        } else {
-            confArr.push(data);
-            socket.join(data);
-            socket.emit('createСonfConfirm', data);
-            //console.log(`User ${user.name} create conference ${data}`);
-        }
-    });
+      switch (type) {
+        case 'user:name':
+          socket.name = data.payload;
+          console.log(`socket.uuid = ${socket.uuid}. socket.name = ${socket.name}`);
+          socket.emit('userNameConfirm');
+          break;
 
-    socket.on('joinConf', data => {
-        const a = confArr.some(item => {
-            return item === data;
-        });
+        case 'user:color':
+          socket.color = data.payload;
+          break; //можно сделать менюшку на клиенте с выбором цвета
+                 //или назначать сервером через require('randomcolor')
 
-        if (a) {
-            socket.join(data);
-            socket.emit('joinConfConfirm', data);
-            //console.log(`User ${user.name} join conference ${data}`);
-        } else {
-            socket.emit('confNotExist');
-        }
-    });
+        case 'user:join':
+            const a = allStates.some(item => { //проверка наличия конференции
+                return item === data.payload;
+            });
 
-    socket.on('alertForConf', () => {
-        let room = Object.keys(socket.rooms)[1];
-        io.to(room).emit('message');
-    });
+            if ( a ) {
+                socket.conf = data.payload;
+                socket.win = false;
+                socket.join(data.payload);
+                socket.emit('joinConfConfirm');
+                console.log(`User ${socket.name} join to conference ${socket.conf}`);
 
-    socket.on('newUser', data => {
-        socket.to(ownerID(socket)).emit('newUserInConf', data);
-    });
+            } else {
+                socket.emit('confNotExist')
+            }
+            break;
 
-    socket.on('ownerStateToAll', data => {
-        let room = Object.keys(socket.rooms)[1];
-        socket.to(room).emit('synchData', data);
-    });
+        case 'conference:name':
+            const b = allStates.some(item => { //проверка уникальности имени конференции
+                return item === data.payload;
+            });
 
-    socket.on('alertForOwner', () => {
-        socket.to(ownerID(socket)).emit('messageForOwner');
-    });
+            if ( b ) {
+                socket.emit('changeСonfName')
+            } else {
+                socket.conf = data.payload;
+                socket.win = true;
+                socket.join(data.payload);
+                allStates.push(data.payload);
+                console.log(allStates);
+                socket.emit('createСonfConfirm');
+                console.log(`User ${socket.name} create conference ${socket.conf}`);
+            }
+            break;
 
-    socket.on('disconnect', () => {
-        io.sockets.emit('user disconnected ID', user.id);
-        usersArr.splice(usersArr.indexOf(user),1);
-        //console.log(`user ${user.name} disconnected ID ${user.id}`);
+        case 'canvas:lock':
+          /* ... */
+          break;
 
-    });
+        case 'canvas:unlock':
+          /* ... */
+          break;
+
+        case 'alert':
+            io.to(socket.conf).emit(`message`);
+            break; //для проверки
+      }
+    })
+
+    .on('disconnect', () => {
+        console.log(`user disconnected. Socket ID ${socket.id}`);
+    })
 });
-
-function ownerID(socket) {
-    let room = Object.keys(socket.rooms)[1];
-    let roomsList = socket.adapter.rooms;
-    let sockList = Object.keys(roomsList[room].sockets);
-    return sockList[0];
-}
