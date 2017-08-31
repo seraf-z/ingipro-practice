90ecd97d1b89934bb21b6893d2c22e9bb223fe13
Oleg Lebedev <oleg.lebedev.v@gmail.com>
2017-08-31 21:20:26 +0300

    2

--- app/server.js
+++ app/server.js
@@ -1,19 +1,140 @@
-const http = require('http');
-const path = require('path');
 const express = require('express');
+const randomColor = require('randomcolor');
+const http = require('http');
+const soketIO = require('socket.io');
+const path = require('path');
 const serveStatic = require('serve-static');
 
-const PORT = 3000;
 const app = express();
+const server = http.createServer(app);
+const io = soketIO(server);
+
+const PORT = 3000;
+const conference = {
+    users: [],
+    state: {},
+};
+
+//app.use(serveStatic(__dirname + '..' + 'build'));
 app.use(serveStatic(path.join(__dirname, '..', 'build')));
 
-const server = http.Server(app);
 server.listen(PORT, () => {
-    // eslint-disable-next-line
-    console.log(`Server started on localhost:${PORT}`);
+    console.log(`server listen ${PORT}`);
 });
 
-const io = require('./socket')(server);
-app.set('io', io);
-// eslint-disable-next-line
-console.log(`!!!`);
+io.sockets.on('connection', (socket) => {
+    let user = {
+        //userID: shortid.generate(),
+        socketID: socket.id,
+        color: randomColor(),
+    };
+
+    socket
+        .on('main', data => {
+
+            let type = data.type;
+            console.dir(`type -> ${data.type}; payload -> ${data.payload}`);
+            console.log(data.payload);
+            switch (type) {
+                case 'user:join':
+                    user.name = data.payload.name;
+                    conference.users.push(user);
+                    let conferenceSync = {
+                        type: 'conference:sync',
+                        payload: {
+                            userList: conference.users,
+                            state: conference.state
+                        }
+                    };
+                    let conferenceJoin = {
+                        type: 'conference:join',
+                        payload: {
+                            userId: user.userID,
+                            name: user.name,
+                            color: user.color
+                        }
+                    };
+                    socket.emit('main', conferenceSync);
+                    socket.broadcast.emit('main', conferenceJoin);
+                    break;
+
+                case 'canvas:lock':
+                    let a = conference.users.some(item => {
+                        return item.owner === true;
+                    });
+
+                    if ( a ) {
+                        let lockDenied = {
+                            type: 'lock:denied',
+                            payload: {
+                                userId: user.userID
+                            }
+                        };
+                        socket.emit('main', lockDenied);
+                    } else {
+                        let lockAccept = {
+                            type: 'lock:accept',
+                            payload: {
+                                userId: user.userID
+                            }
+                        };
+                        let conferenceLock = {
+                            type: 'conference:lock',
+                            payload: {
+                                userId: user.userID,
+                                name: user.name,
+                                color: user.color
+                            }
+                        };
+                        socket.emit('main', lockAccept);
+                        socket.broadcast.emit('main', conferenceLock);
+                    }
+                    break;
+
+                case 'canvas:unlock':
+                    user.owner = false;
+                    break;
+
+                case 'state:upload':
+                    if (user.owner === true) {
+                        conference.state = data.payload.state;
+                        let stateChange = {
+                            type: 'state:change',
+                            payload: {
+                                state: conference.state
+                            }
+                        };
+                        socket.broadcast.emit('main', stateChange);
+                    }
+                    break;
+
+                case 'chat:message':
+                    let chatMessage = {
+                        type: 'chat:message',
+                        payload: {
+                            userName: user.name,
+                            message: data.payload.message
+                        }
+                    };
+                    io.emit('main', chatMessage);
+                    break;
+            }
+        })
+
+        .on('disconnect', () => {
+            console.log(`user ${user.name} disconnect`);
+            let conferenceLeave = {
+                type: 'conference:leave',
+                payload: {
+                    userId: user.userID,
+                    name: user.name,
+                    color: user.color,
+                }
+            };
+            socket.broadcast.emit('main', conferenceLeave);
+            let index = conference.users.indexOf(user);
+            conference.users.splice(index, 1);
+            console.log(conference.users);
+
+        });
+});
